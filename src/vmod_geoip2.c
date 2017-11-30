e86a56871476b78f28690f7818432bacce35859d
Federico G. Schwindt <fgsch@lodoss.net>
2017-11-30 12:41:05 +0000

    Sync with devel
    Drop __match_proto__
    
    I don't use flexelint and this macro has been renamed so let's just
    remove it.

--- src/vmod_geoip2.c
+++ src/vmod_geoip2.c
@@ -66,7 +66,7 @@ vslv(VRT_CTX, enum VSL_tag_e tag, const char *fmt, ...)
 	va_end(ap);
 }
 
-VCL_VOID __match_proto__(td_geoip2_geoip2__init)
+VCL_VOID
 vmod_geoip2__init(VRT_CTX, struct vmod_geoip2_geoip2 **vpp,
     const char *vcl_name, VCL_STRING filename)
 {
@@ -96,7 +96,7 @@ vmod_geoip2__init(VRT_CTX, struct vmod_geoip2_geoip2 **vpp,
 	vp->mmdb = mmdb;
 }
 
-VCL_VOID __match_proto__(td_geoip2_geoip2__fini)
+VCL_VOID
 vmod_geoip2__fini(struct vmod_geoip2_geoip2 **vpp)
 {
 	struct vmod_geoip2_geoip2 *vp;
@@ -111,7 +111,7 @@ vmod_geoip2__fini(struct vmod_geoip2_geoip2 **vpp)
 	FREE_OBJ(vp);
 }
 
-VCL_STRING __match_proto__(td_geoip2_geoip2_lookup)
+VCL_STRING
 vmod_geoip2_lookup(VRT_CTX, struct vmod_geoip2_geoip2 *vp,
     VCL_STRING path, VCL_IP addr)
 {
